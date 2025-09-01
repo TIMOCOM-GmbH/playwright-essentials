33454695a1cc8a813bafb139a91297454be7d08a
Markus Bong <mbong@timocom.com>
2025-09-01 13:41:55 +0200

    chore(ci): relax commitlint for semantic-release and shorten release commit message

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,5 +3,9 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 100],
+    // Allow long lines in commit body to avoid CI failures (CHANGELOG snippets, links, etc.)
+    'body-max-line-length': [0, 'always'],
   },
+  // Ignore semantic-release commit messages like "chore(release): 1.0.0 [skip ci]"
+  ignores: [message => /chore\(release\): \d+\.\d+\.\d+/.test(message)],
 }
