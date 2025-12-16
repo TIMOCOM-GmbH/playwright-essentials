44e1cdebfde070d86bd050bcf37d169386c347cf
Markus Bong <mbong@timocom.com>
2025-12-16 14:01:31 +0100

    feat: add functions to block EUM requests and all analytics requests

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,19 @@ import type { Page } from '@playwright/test'
 export async function blockPendo(page: Page): Promise<void> {
   await page.route('https://cdn.eu.pendo.io/**', route => route.abort())
 }
+
+/**
+ * Blocks all eum requests for the given page.
+ * @param page Playwright Page instance
+ */
+export async function blockEum(page: Page): Promise<void> {
+  await page.route('https://eum.timocom.com/**', route => route.abort())
+}
+
+/** Blocks all analytics requests (Pendo and EUM) for the given page.
+ * @param page Playwright Page instance
+ */
+export async function blockAllAnalytics(page: Page): Promise<void> {
+  await blockPendo(page)
+  await blockEum(page)
+}
